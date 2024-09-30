8546449d1ab9a86f4aa637ce903a2e203fa96526
Diane Dushimimana <dushimediane12@gmail.com>
2024-09-30 17:43:37 +0200

    all

--- src/routes/registrationRoutes.js
+++ src/routes/registrationRoutes.js
@@ -1,5 +1,6 @@
 // routes/registrationRoutes.js
 const express = require('express');
+
 const Registration = require('../models/Registration'); // Ensure the path is correct
 const router = express.Router();
 
@@ -14,5 +15,21 @@ router.get('/registrations', async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch registrations' });
   }
 });
+router.post('/register', async (req, res) => {
+  try {
+    // Create a new Registration object from the request body
+    const newRegistration = new Registration(req.body);
+    
+    // Save the new registration data to the database
+    await newRegistration.save();
+    
+    // Send a success response
+    res.status(201).json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error("Error registering:", error);
+    res.status(500).json({ message: 'Failed to register' }); // Handle any errors
+  }
+});
+
 
 module.exports = router;
